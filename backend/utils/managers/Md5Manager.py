4ffa769128e1262939d6388bed3230633fe91b51
mhavryshko <115162300+mhavryshko@users.noreply.github.com>
2025-05-06 11:08:22 +0300

    Add files via upload

---
+++ backend/utils/managers/Md5Manager.py
@@ -0,0 +1,18 @@
+import hashlib
+import random
+import uuid
+from datetime import datetime
+from LabBackend.Utils.Interfaces import IIdentifiable
+
+
+class Md5Manager(IIdentifiable):
+    def __init__(self):
+        self.rand = random.Random()
+
+    def generate_id(self) -> str:
+        current_time = datetime.utcnow()
+        random_value = self.rand.random()
+        unique_data = f"{current_time.timestamp()}{random_value}{uuid.uuid4()}"
+
+        md5_hash = hashlib.md5(unique_data.encode('utf-8')).hexdigest()
+        return md5_hash
